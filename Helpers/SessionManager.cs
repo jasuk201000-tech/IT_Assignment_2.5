183cdaf812f5994ef93ef2291dcc5ff0514cbf67
jasu <jasuk201000@gmail.com>
2026-05-09 08:29:59 +1000

    fixing the earlier issues

---
+++ Helpers/SessionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IT_Assessment_2.Models;
+
+namespace IT_Assessment_2.Helpers
+{
+    public class SessionManager
+    {
+
+        public Staff? => _CurrentUser;
+
+        public static Staff? _CurrentUser => _CurrentUser;
+
+        public bool IsLoggedIn { get; set; }
+
+        
+
+
+    }
+}
---
+++ Models/Staff.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT_Assessment_2.Models
+{
+    public class Staff
+    {
+        // enumerates staff role for easier implementation later on
+        public enum UserRole
+            {
+            Admin = 1,
+            Manager = 2,
+            Cashier = 3
+            }
+
+
+    }
+}
