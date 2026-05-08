221eee865c63a9da3d08c5408acf57ec0e240448
jasu <jasuk201000@gmail.com>
2026-05-09 08:06:38 +1000

    defining reusable design features and parameters for forms

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2105, 953);
+            this.ClientSize = new System.Drawing.Size(2161, 1056);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
