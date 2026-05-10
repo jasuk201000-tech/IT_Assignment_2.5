c3251957075a6ec2ec3ffd22a8bd0569a49a238b
jasu <jasuk201000@gmail.com>
2026-05-10 12:29:48 +1000

    adding more UI components to Dashboard

--- Forms/DashboardForm1.Designer.cs
+++ Forms/DashboardForm1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardForm1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
@@ -36,12 +37,42 @@
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.LogoBtn = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.welcomeLbl = new System.Windows.Forms.Label();
+            this.dateLbl = new System.Windows.Forms.Label();
+            this.salesPnl = new System.Windows.Forms.Panel();
+            this.ordersPnl = new System.Windows.Forms.Panel();
+            this.lowstockPnl = new System.Windows.Forms.Panel();
+            this.returnsPnl = new System.Windows.Forms.Panel();
+            this.salesLbl = new System.Windows.Forms.Label();
+            this.ordersLbl = new System.Windows.Forms.Label();
+            this.lowstockLbl = new System.Windows.Forms.Label();
+            this.returnsLbl = new System.Windows.Forms.Label();
+            this.intSalesLbl = new System.Windows.Forms.Label();
+            this.intOrdersLbl = new System.Windows.Forms.Label();
+            this.lowstockintLbl = new System.Windows.Forms.Label();
+            this.returnsintLbl = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.salesPnl.SuspendLayout();
+            this.ordersPnl.SuspendLayout();
+            this.lowstockPnl.SuspendLayout();
+            this.returnsPnl.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -55,7 +86,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(-3, -5);
+            this.pictureBox2.Location = new System.Drawing.Point(-29, 216);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(2131, 351);
             this.pictureBox2.TabIndex = 3;
@@ -64,7 +95,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(731, -5);
+            this.pictureBox1.Location = new System.Drawing.Point(708, 216);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(2131, 351);
             this.pictureBox1.TabIndex = 4;
@@ -73,7 +104,7 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(1459, -5);
+            this.pictureBox3.Location = new System.Drawing.Point(1443, 216);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(2131, 351);
             this.pictureBox3.TabIndex = 5;
@@ -107,12 +138,243 @@
             this.LogoBtn.Text = "𝖆𝖒𝖆𝖓𝖊";
             this.LogoBtn.UseVisualStyleBackColor = false;
             // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(982, 105);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(147, 50);
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // welcomeLbl
+            // 
+            this.welcomeLbl.AutoSize = true;
+            this.welcomeLbl.Font = new System.Drawing.Font("Cambria", 15F);
+            this.welcomeLbl.Location = new System.Drawing.Point(817, 175);
+            this.welcomeLbl.Name = "welcomeLbl";
+            this.welcomeLbl.Size = new System.Drawing.Size(496, 47);
+            this.welcomeLbl.TabIndex = 8;
+            this.welcomeLbl.Text = "welcome *insert username*";
+            // 
+            // dateLbl
+            // 
+            this.dateLbl.AutoSize = true;
+            this.dateLbl.Font = new System.Drawing.Font("Cambria", 12F);
+            this.dateLbl.Location = new System.Drawing.Point(975, 234);
+            this.dateLbl.Name = "dateLbl";
+            this.dateLbl.Size = new System.Drawing.Size(166, 37);
+            this.dateLbl.TabIndex = 9;
+            this.dateLbl.Text = "insert date";
+            // 
+            // salesPnl
+            // 
+            this.salesPnl.Controls.Add(this.intSalesLbl);
+            this.salesPnl.Controls.Add(this.salesLbl);
+            this.salesPnl.Location = new System.Drawing.Point(146, 366);
+            this.salesPnl.Name = "salesPnl";
+            this.salesPnl.Size = new System.Drawing.Size(321, 187);
+            this.salesPnl.TabIndex = 10;
+            // 
+            // ordersPnl
+            // 
+            this.ordersPnl.Controls.Add(this.intOrdersLbl);
+            this.ordersPnl.Controls.Add(this.ordersLbl);
+            this.ordersPnl.Location = new System.Drawing.Point(613, 366);
+            this.ordersPnl.Name = "ordersPnl";
+            this.ordersPnl.Size = new System.Drawing.Size(321, 187);
+            this.ordersPnl.TabIndex = 11;
+            // 
+            // lowstockPnl
+            // 
+            this.lowstockPnl.Controls.Add(this.lowstockintLbl);
+            this.lowstockPnl.Controls.Add(this.lowstockLbl);
+            this.lowstockPnl.Location = new System.Drawing.Point(1195, 366);
+            this.lowstockPnl.Name = "lowstockPnl";
+            this.lowstockPnl.Size = new System.Drawing.Size(321, 187);
+            this.lowstockPnl.TabIndex = 12;
+            // 
+            // returnsPnl
+            // 
+            this.returnsPnl.Controls.Add(this.returnsintLbl);
+            this.returnsPnl.Controls.Add(this.returnsLbl);
+            this.returnsPnl.Location = new System.Drawing.Point(1689, 366);
+            this.returnsPnl.Name = "returnsPnl";
+            this.returnsPnl.Size = new System.Drawing.Size(321, 187);
+            this.returnsPnl.TabIndex = 13;
+            // 
+            // salesLbl
+            // 
+            this.salesLbl.AutoSize = true;
+            this.salesLbl.Font = new System.Drawing.Font("Cambria", 11F);
+            this.salesLbl.Location = new System.Drawing.Point(36, 28);
+            this.salesLbl.Name = "salesLbl";
+            this.salesLbl.Size = new System.Drawing.Size(187, 36);
+            this.salesLbl.TabIndex = 14;
+            this.salesLbl.Text = "Todays Sales:";
+            // 
+            // ordersLbl
+            // 
+            this.ordersLbl.AutoSize = true;
+            this.ordersLbl.Font = new System.Drawing.Font("Cambria", 11F);
+            this.ordersLbl.Location = new System.Drawing.Point(24, 28);
+            this.ordersLbl.Name = "ordersLbl";
+            this.ordersLbl.Size = new System.Drawing.Size(112, 36);
+            this.ordersLbl.TabIndex = 15;
+            this.ordersLbl.Text = "Orders:";
+            // 
+            // lowstockLbl
+            // 
+            this.lowstockLbl.AutoSize = true;
+            this.lowstockLbl.Font = new System.Drawing.Font("Cambria", 11F);
+            this.lowstockLbl.Location = new System.Drawing.Point(29, 28);
+            this.lowstockLbl.Name = "lowstockLbl";
+            this.lowstockLbl.Size = new System.Drawing.Size(153, 36);
+            this.lowstockLbl.TabIndex = 16;
+            this.lowstockLbl.Text = "Low stock:";
+            // 
+            // returnsLbl
+            // 
+            this.returnsLbl.AutoSize = true;
+            this.returnsLbl.Font = new System.Drawing.Font("Cambria", 11F);
+            this.returnsLbl.Location = new System.Drawing.Point(28, 28);
+            this.returnsLbl.Name = "returnsLbl";
+            this.returnsLbl.Size = new System.Drawing.Size(117, 36);
+            this.returnsLbl.TabIndex = 17;
+            this.returnsLbl.Text = "Returns";
+            // 
+            // intSalesLbl
+            // 
+            this.intSalesLbl.AutoSize = true;
+            this.intSalesLbl.Font = new System.Drawing.Font("Cambria", 10F);
+            this.intSalesLbl.Location = new System.Drawing.Point(46, 89);
+            this.intSalesLbl.Name = "intSalesLbl";
+            this.intSalesLbl.Size = new System.Drawing.Size(80, 32);
+            this.intSalesLbl.TabIndex = 16;
+            this.intSalesLbl.Text = "$////";
+            // 
+            // intOrdersLbl
+            // 
+            this.intOrdersLbl.AutoSize = true;
+            this.intOrdersLbl.Font = new System.Drawing.Font("Cambria", 10F);
+            this.intOrdersLbl.Location = new System.Drawing.Point(35, 79);
+            this.intOrdersLbl.Name = "intOrdersLbl";
+            this.intOrdersLbl.Size = new System.Drawing.Size(66, 32);
+            this.intOrdersLbl.TabIndex = 17;
+            this.intOrdersLbl.Text = "////";
+            // 
+            // lowstockintLbl
+            // 
+            this.lowstockintLbl.AutoSize = true;
+            this.lowstockintLbl.Font = new System.Drawing.Font("Cambria", 10F);
+            this.lowstockintLbl.Location = new System.Drawing.Point(40, 89);
+            this.lowstockintLbl.Name = "lowstockintLbl";
+            this.lowstockintLbl.Size = new System.Drawing.Size(66, 32);
+            this.lowstockintLbl.TabIndex = 18;
+            this.lowstockintLbl.Text = "////";
+            // 
+            // returnsintLbl
+            // 
+            this.returnsintLbl.AutoSize = true;
+            this.returnsintLbl.Font = new System.Drawing.Font("Cambria", 10F);
+            this.returnsintLbl.Location = new System.Drawing.Point(40, 89);
+            this.returnsintLbl.Name = "returnsintLbl";
+            this.returnsintLbl.Size = new System.Drawing.Size(66, 32);
+            this.returnsintLbl.TabIndex = 19;
+            this.returnsintLbl.Text = "////";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DarkGray;
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Location = new System.Drawing.Point(1466, 614);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(600, 425);
+            this.panel4.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Cambria", 8F);
+            this.button1.Location = new System.Drawing.Point(40, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(312, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Build Order";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Cambria", 8F);
+            this.button2.Location = new System.Drawing.Point(40, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(312, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Add product";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Cambria", 8F);
+            this.button3.Location = new System.Drawing.Point(40, 259);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(312, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "View stock";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Cambria", 8F);
+            this.button4.Location = new System.Drawing.Point(40, 337);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(312, 42);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Reports";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.LightGray;
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(1466, 614);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(600, 78);
+            this.flowLayoutPanel1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 10F);
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(211, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quick Navigation";
+            // 
             // DashboardForm1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(2129, 1176);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.returnsPnl);
+            this.Controls.Add(this.lowstockPnl);
+            this.Controls.Add(this.ordersPnl);
+            this.Controls.Add(this.salesPnl);
+            this.Controls.Add(this.dateLbl);
+            this.Controls.Add(this.welcomeLbl);
+            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox1);
@@ -125,7 +387,20 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.salesPnl.ResumeLayout(false);
+            this.salesPnl.PerformLayout();
+            this.ordersPnl.ResumeLayout(false);
+            this.ordersPnl.PerformLayout();
+            this.lowstockPnl.ResumeLayout(false);
+            this.lowstockPnl.PerformLayout();
+            this.returnsPnl.ResumeLayout(false);
+            this.returnsPnl.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -138,5 +413,28 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button LogoBtn;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label welcomeLbl;
+        private System.Windows.Forms.Label dateLbl;
+        private System.Windows.Forms.Panel salesPnl;
+        private System.Windows.Forms.Label salesLbl;
+        private System.Windows.Forms.Panel ordersPnl;
+        private System.Windows.Forms.Label ordersLbl;
+        private System.Windows.Forms.Panel lowstockPnl;
+        private System.Windows.Forms.Label lowstockLbl;
+        private System.Windows.Forms.Panel returnsPnl;
+        private System.Windows.Forms.Label returnsLbl;
+        private System.Windows.Forms.Label intSalesLbl;
+        private System.Windows.Forms.Label intOrdersLbl;
+        private System.Windows.Forms.Label lowstockintLbl;
+        private System.Windows.Forms.Label returnsintLbl;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
     }
 }
